8111a05f25487eb897008aa5e15a54209e703d6f
Slaine-Skelton <bretthenning@hotmail.com>
2018-04-11 15:58:06 +0100

    11-04-2018

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+    private Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+        myAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("space"))
+        {
+            myAnimator.SetTrigger("hit");
+            gameObject.transform.localScale = new Vector3(0.09f, 0.09f);
+            StartCoroutine(delayFall());
+        }
+
+        if (Input.GetKeyUp("space"))
+        {
+            myAnimator.ResetTrigger("hit");
+            gameObject.transform.localScale = new Vector3(0.08f, 0.08f);
+        }
+    }
+
+    IEnumerator delayFall()
+    {
+        yield return new WaitForSeconds(0.4f);
+        gameObject.transform.localScale = new Vector3(0.08f, 0.08f);
+    }
+}
---
+++ Assets/Scripts/CollectMatches.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectMatches : MonoBehaviour {
+
+    public int matchCount = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        tag = collision.tag;
+
+        if(tag == "match")
+        {
+            matchCount++;
+            Destroy(collision.gameObject);
+        }
+    }
+}
